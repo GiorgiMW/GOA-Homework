6de72121fb89261852bb90df0ae84ac4b1853a90
GiorgiMW <giorgimw12@gmail.com>
2024-03-11 12:42:46 +0400

    commit

---
+++ day 2/homework.py
@@ -0,0 +1,3 @@
+print("I met and contacted: Atuka Khuskivadze, razmis lideri:Data diasamidze, gio gagnidze  " )
+#რას ვფიქრობ GOAზე
+print("i think that GOA is the best coding academy, with a best community")
