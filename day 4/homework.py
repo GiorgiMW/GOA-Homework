7032f1a0b935709c2e269cc61b4f88234ab70874
GiorgiMW <giorgimw12@gmail.com>
2024-03-12 21:06:40 +0400

    commit

---
+++ day 4/homework.py
@@ -0,0 +1,7 @@
+#keeping user's name and surname
+name = input("Enter your name here: ")
+
+surname = input("Enter your surname here: ")
+
+#display user's name and surname
+print("Hello your name is " + name + " and your password is " + surname)
