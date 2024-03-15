fa3910b0e08f6e61cd377ee8b844cbe60f6e7076
GiorgiMW <giorgimw12@gmail.com>
2024-03-15 16:56:47 +0400

    commit

---
+++ day 10/homework.py
@@ -0,0 +1,6 @@
+#Create a list of programming languages, 
+#enter 5 programming languages. Print the entire list first, then the last item.
+language_list = ["C#", "C++", "JS", "Python", "SWIFT"]
+
+print(language_list)
+print(language_list[4])
