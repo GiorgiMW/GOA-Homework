6d0bc7b504dacf8060075d108c2f3ece541f4a6d
GiorgiMW <giorgimw12@gmail.com>
2024-03-15 20:05:24 +0400

    commit

---
+++ day 11/homework.py
@@ -0,0 +1,49 @@
+#Create a list passing in 10 integers.
+number_list = [3, 5, 10, 16, 20, 2, 13, 27, 29, 18]
+
+#iterate through the list and multiply/divide/add/subtract each of them
+print(number_list[9] + number_list[0])
+print(number_list[9] + number_list[1])
+print(number_list[9] + number_list[2])
+print(number_list[9] + number_list[3])
+print(number_list[9] + number_list[4])
+print(number_list[9] + number_list[5])
+print(number_list[9] + number_list[6])
+print(number_list[9] + number_list[7])
+print(number_list[9] + number_list[8])
+print(number_list[9] + number_list[9])
+
+print(number_list[9] - number_list[0])
+print(number_list[9] - number_list[1])
+print(number_list[9] - number_list[2])
+print(number_list[9] - number_list[3])
+print(number_list[9] - number_list[4])
+print(number_list[9] - number_list[5])
+print(number_list[9] - number_list[6])
+print(number_list[9] - number_list[7])
+print(number_list[9] - number_list[8])
+print(number_list[9] - number_list[9])
+
+print(number_list[9] * number_list[0])
+print(number_list[9] * number_list[1])
+print(number_list[9] * number_list[2])
+print(number_list[9] * number_list[3])
+print(number_list[9] * number_list[4])
+print(number_list[9] * number_list[5])
+print(number_list[9] * number_list[6])
+print(number_list[9] * number_list[7])
+print(number_list[9] * number_list[8])
+print(number_list[9] * number_list[9])
+
+print(number_list[9] / number_list[0])
+print(number_list[9] / number_list[1])
+print(number_list[9] / number_list[2])
+print(number_list[9] / number_list[3])
+print(number_list[9] / number_list[4])
+print(number_list[9] / number_list[5])
+print(number_list[9] / number_list[6])
+print(number_list[9] / number_list[7])
+print(number_list[9] / number_list[8])
+print(number_list[9] / number_list[9])
+
+
