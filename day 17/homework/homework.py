636be13fd35e57b7809250b0bf2355b15aa0e61f
GiorgiMW <giorgimw12@gmail.com>
2024-04-03 23:12:55 +0400

    commit

---
+++ day 17/homework/homework.py
@@ -0,0 +1,6 @@
+def func(name):
+    return (name.lower(), name.upper(), name.capitalize(), name.find("G"))
+
+name1 = "Goa is the best"
+    
+print(func(name1))
---
+++ day 17/homework/homework2.py
@@ -0,0 +1,5 @@
+def list(listn,replace):
+    listn[x] = "Apple"
+    return listn
+x = int(input("Enter which you want to replace: "))
+print(list(["Banana", "fruit","home","clock"], x))
